a620322858c6e024109819ad011822e38c90f906
Kevin Daysal <kdaysal123@gmail.com>
2022-03-19 11:26:59 -0400

    Added boilerplate code from exercise to index.js, and updated project file structure

---
+++ index.js
@@ -0,0 +1,39 @@
+//Utilize Express framework for this app
+const express = require('express');
+const app = express();
+
+let topBooks = [
+  {
+    title: 'Harry Potter and the Sorcerer\'s Stone',
+    author: 'J.K. Rowling'
+  },
+  {
+    title: 'Lord of the Rings',
+    author: 'J.R.R. Tolkien'
+  },
+  {
+    title: 'Twilight',
+    author: 'Stephanie Meyer'
+  }
+];
+
+// GET requests - these are endpoints (or routes)
+// The structure is: app.METHOD(PATH, HANDLER)
+// 'app' is an instance of express()
+app.get('/', (req, res) => {
+  res.send('Welcome to my book club!');
+});
+
+app.get('/documentation', (req, res) => {                  
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.get('/books', (req, res) => {
+  res.json(topBooks);
+});
+
+
+// listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
