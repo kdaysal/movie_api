bf390e8a398cdec7f77254ff64c8006162751721
Kevin Daysal <kdaysal123@gmail.com>
2022-03-23 22:33:01 -0400

    implemented 3 more routes and installed uuid
    implemented get route for /movies/genre/:genreName endpoint.
    
    implemented get route for /movies/directors/:directorName endpoint.
    
    installed uuid (npm install uuid)
    
    implemented post route for /users

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 const express = require('express'),
   morgan = require('morgan'), //custom middleware for loging request details to log.txt
   bodyParser = require('body-parser'), //middleware for error-handling
-  methodOverride = require('method-override'); //middleware for error-handling
+  methodOverride = require('method-override'), //middleware for error-handling
+  uuid = require('uuid');
 const res = require('express/lib/response');
 
 const app = express();
@@ -109,7 +110,7 @@ let movies = [
     "director": {
       "name": "Jonathan Lynn",
       "bio": "Jonathan Lynn (born 3 April 1943) is an English stage and film director, producer, writer, and actor. He is known for directing comedy films such as Clue (1985), Nuns on the Run (1990), My Cousin Vinny (1992) and The Whole Nine Yards (2000). He co-created and co-wrote the television series Yes Minister (1980–1984, 2013) and Yes, Prime Minister (1986–1988).",
-      "birth year":1943 
+      "birth year": 1943
     },
     "imageURL": "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/nk5XnrehDowm5Q5Wr8hsObiD6C1.jpg",
     "featured": false
@@ -124,7 +125,7 @@ let movies = [
     "director": {
       "name": "Martin Campbell",
       "bio": "Martin Campbell (born 24 October 1943) is a New Zealand film and television director based in the United Kingdom. He directed the British miniseries Edge of Darkness (1985), for which he won a BAFTA, The Mask of Zorro (1998), and the James Bond films GoldenEye (1995) and Casino Royale (2006).",
-      "birth year":1943 
+      "birth year": 1943
     },
     "imageURL": "https://en.wikipedia.org/wiki/Martin_Campbell#/media/File:Martin_Campbell.jpg",
     "featured": true
@@ -132,6 +133,20 @@ let movies = [
 ]
 
 app.use(express.static('public')); //serves static files from the 'public' folder
+
+// CREATE (POST)
+app.post('/users', (req, res) => {
+  const newUser = req.body //the body-parser package is what allows this line to work
+
+  //check to make sure the new user object has a name
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser);
+  } else {
+    res.status(400).send('please enter a name for the user');
+  }
+})
 
 // GET requests - these are endpoints (or routes)
 // The structure is: app.METHOD(PATH, HANDLER)
@@ -146,19 +161,46 @@ app.get('/movies', (req, res) => {
 });
 
 //return data (description, genre, director, image URL, featured-or-not) about a movie -by title-
-//This assigns whatever the user inputs after the 'movies/' to the request (req) as a parameter
+//this assigns whatever the user inputs after the 'movies/' to the request (req) as a parameter called 'title'
 app.get('/movies/:title', (req, res) => {
   // const title = req.params.title; refactored below in { object destructuring } format
   const { title } = req.params;
   const movie = movies.find(movie => movie.title === title);
 
+  if (movie) {
+    res.status(200).json(movie);
+  }
+  else {
+    res.status(400).send('no such movie in the database');
+  }
+});
 
-if (movie) {
-  res.status(200).json(movie);
-}
-else {
-  res.status(400).send('no such movie in the database');
-}
+//return data about a genre (description) -by genre name/title- (e.g., "Horror")
+app.get('/movies/genre/:genreName', (req, res) => {
+  // const title = req.params.title; refactored below in { object destructuring } format
+  const { genreName } = req.params;
+  const genre = movies.find(movie => movie.genre.name === genreName).genre; //the '.genre' at the end of the find() method is accessing just the genre data (via dot notation)
+
+  if (genre) {
+    res.status(200).json(genre);
+  }
+  else {
+    res.status(400).send('no such genre in the database');
+  }
+});
+
+//return data about a director (bio, birth year, death year) -by name-
+app.get('/movies/directors/:directorName', (req, res) => {
+  // const title = req.params.title; refactored below in { object destructuring } format
+  const { directorName } = req.params;
+  const director = movies.find(movie => movie.director.name === directorName).director; 
+
+  if (director) {
+    res.status(200).json(director);
+  }
+  else {
+    res.status(400).send('no such genre in the database');
+  }
 });
 
 //error handling - must come last in chain of middleware but before app.listen()
