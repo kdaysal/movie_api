8199625929dd3d454be2951c84f0ea4a9f913a5d
Kevin Daysal <kdaysal123@gmail.com>
2022-04-03 19:49:00 -0400

    Implemented register (add) new user using Mongo Model
    First working iteration of the process, and verified with Postman that 2 new users were added to my users collection.
    
    Also updated documentation.html file accordingly

--- index.js
+++ index.js
@@ -87,18 +87,40 @@ let movies = [
 app.use(express.static('public')); //serves static files from the 'public' folder
 
 // CREATE - POST - Allow new users to register)
+//Add a user
+/* We’ll expect JSON in this format
+{
+  ID: Integer,
+  Username: String,
+  Password: String,
+  Email: String,
+  Birthday: Date
+}*/
 app.post('/users', (req, res) => {
-  const newUser = req.body //the body-parser package is what allows this line to work
-
-  //check to make sure the new user object has a name
-  if (newUser.name) {
-    newUser.id = uuid.v4();
-    users.push(newUser);
-    res.status(201).json(newUser);
-  } else {
-    res.status(400).send('please enter a name for the user');
-  }
-})
+  Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + 'already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) =>{res.status(201).json(user) })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        })
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
 
 //UPDATE - PUT - Allow users to update their existing user info (username) -by id-
 app.put('/users/:id', (req, res) => {
